0c8f6ef94433cb5dc6fb77b0e0a315714dbcb733
Blaise Laflamme <blaise@laflamme.org>
2012-03-12 21:23:49 -0700

    refactored views and switched to mako templates

--- pycomm/__init__.py
+++ pycomm/__init__.py
@@ -10,7 +10,7 @@ def main(global_config, **settings):
     DBSession.configure(bind=engine)
     config = Configurator(settings=settings)
     config.add_static_view('static', 'static', cache_max_age=3600)
-    config.add_route('home', '/')
+    config.include('pycomm.views')
     config.scan()
     return config.make_wsgi_app()
 
---
+++ pycomm/views/events.py
@@ -0,0 +1,10 @@
+from pyramid.view import view_config
+
+from ..models import DBSession
+from ..models.events import Event
+
+
+@view_config(route_name='home', renderer='pycomm:templates/home.mako')
+def home(request):
+    one = DBSession.query(Event).filter(Event.name=='one').first()
+    return {'one':one, 'project':'pycomm'}
