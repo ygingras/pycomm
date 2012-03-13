8039c0a9d2f0e0bbb400d7d8e4b85143dab3ddec
Audrey Roy <audreyr@gmail.com>
2012-03-12 22:48:22 -0700

    First pass at add-event form.

--- pycomm/views/events.py
+++ pycomm/views/events.py
@@ -2,9 +2,36 @@ from pyramid.view import view_config
 
 from ..models import DBSession
 from ..models.events import Event
-
+from ..forms.event_forms import EventAddForm
 
 @view_config(route_name='home', renderer='pycomm:templates/home.mako')
 def home(request):
     one = DBSession.query(Event).first()
     return {'one':one, 'project':'pycomm'}
+
+@view_config(route_name='add-event', request_method='GET', renderer='pycomm:templates/add-event.mako')
+def add(request):
+    form = EventAddForm(request.params)
+    return {'form':form}
+
+@view_config(route_name='add-event', request_method='POST', renderer='pycomm:templates/add-event.mako')
+def save(request):
+    params = request.POST
+    form = EventAddForm(params)
+    if params:
+        if form.validate():
+            event = Event()
+            form.populate_obj(event)
+            DBSession.add(event)
+            DBSession.flush()
+            request.session.flash(
+                u'Event successfully added.',
+                queue='success',
+            )
+        else:
+            request.session.flash(
+                u'Event not added.',
+                queue='error',
+            )
+    return {'form':form}
+
--- setup.py
+++ setup.py
@@ -14,6 +14,7 @@ requires = [
     'pyramid_debugtoolbar',
     'zope.sqlalchemy',
     'waitress',
+    'WTForms==1.0.1',
     ]
 
 setup(name='pycomm',
